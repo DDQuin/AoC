ba101c02a0fcdb7958bc6d6b08a99bc3bd196752
DDQuin <ddquin420@gmail.com>
2023-11-22 19:10:54 +0000

    day16 usedd help

--- day16.go
+++ day16.go
@@ -1,9 +1,179 @@
-//Here's my approach:
+package main
 
-// calculate distances between all valve pairs
+// Taken from https://github.com/bvandewalle/aoc2022/blob/main/16/main.go
 
-// write a recursive function that tells me what the maximum value is that can be gained by opening remaining valves, with a certain time left, when I'm at a given valve, and when there's a given set of valves I may not open anymore (those are my method parameters). To calculate this value, I loop over all non-broken valves (15 in my input) that may still be opened. That's the one I'll move to and open next. Recurse on those. The calculations are straightforward (use the calculated distances) - just be careful with all the details.
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
 
-// For me this ran in a few milliseconds (using C#), which was fast enough to do part 2 with a relatively brute force approach (consider all possible splits of 14 relevant valves between me and the elephant: about 16000...)
+	"github.com/RyanCarrier/dijkstra"
+	"github.com/ernestosuarez/itertools"
+)
 
-// This was by far the hardest puzzle for me, btw...
+func Day16() {
+	file, _ := os.Open("resources/day16input.txt")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var input []string
+
+	for scanner.Scan() {
+		v := scanner.Text()
+		input = append(input, v)
+	}
+
+	parts(input, false)
+	parts(input, true)
+}
+
+func parts(input []string, part2 bool) {
+	reachability := map[string][]string{}
+	pressure := map[string]int{}
+
+	// Used for a mapping from string to an index for the dijkstra lib
+	mappingInt := map[string]int{}
+	count := 0
+
+	for _, iv := range input {
+		iv := strings.TrimPrefix(iv, "Valve ")
+		a := strings.Split(iv, " has flow rate=")
+		b := strings.Split(a[1], "; tunnels lead to valve")
+		if len(b) == 1 {
+			b = strings.Split(a[1], "; tunnel leads to valve")
+		}
+		rate, _ := strconv.Atoi(b[0])
+
+		b[1] = strings.TrimPrefix(b[1], "s ")
+		b[1] = strings.TrimPrefix(b[1], " ")
+		c := strings.Split(b[1], ", ")
+
+		reachability[a[0]] = []string{}
+		for _, v := range c {
+			reachability[a[0]] = append(reachability[a[0]], v)
+		}
+		pressure[a[0]] = rate
+
+		mappingInt[a[0]] = count
+		count++
+	}
+
+	tunnels := []string{}
+	// Nifty  trick: Don't consider the destination with a valve of value zero.
+	// This reduces the search space by A LOT
+	for k, v := range pressure {
+		if v != 0 {
+			tunnels = append(tunnels, k)
+		}
+	}
+
+	// PreCalculate Dijkstra as a full matrix. To save time as those results will be used many times
+	matrix := calcReachabilityMatrix(reachability, mappingInt)
+
+	maxPressure := 0
+	mu := sync.Mutex{}
+
+	if !part2 {
+		// Part1: Simply brute force on the problem space...
+		maxPressure = solveRecur(matrix, pressure, 0, 0, 0, "AA", tunnels, 30)
+	} else {
+		// Part2: Create all the possible combination of the destination and their opposite
+		// Then run both brute force in parallels
+		// Nifty trick: Only do the combination to half the length of the problem space as the opposite will take care of the mirroring solution
+		// Running in parrallell to make it slightly faster
+		for i := 1; i < len(tunnels)/2; i++ {
+			for v := range itertools.CombinationsStr(tunnels, i) {
+				vcopy := []string{}
+				vcopy = append([]string(nil), v...)
+				go func() {
+					maxPressure2a := solveRecur(matrix, pressure, 0, 0, 0, "AA", vcopy, 26)
+					maxPressure2b := solveRecur(matrix, pressure, 0, 0, 0, "AA", createOpposite(tunnels, vcopy), 26)
+					mu.Lock()
+					if maxPressure2a+maxPressure2b > maxPressure {
+						maxPressure = maxPressure2a + maxPressure2b
+					}
+					mu.Unlock()
+				}()
+			}
+		}
+	}
+
+	fmt.Println(maxPressure)
+}
+
+func solveRecur(matrix map[string]map[string]int, pressures map[string]int, currentTime int, currentPressure int, currentFlow int, currentTunnel string, remaining []string, limit int) int {
+	// The score if no other valves are being opened before the $limit time
+	nScore := currentPressure + (limit-currentTime)*currentFlow
+	max := nScore
+
+	for _, v := range remaining {
+		distanceAndOpen := matrix[currentTunnel][v] + 1
+		if currentTime+distanceAndOpen < limit {
+			newTime := currentTime + distanceAndOpen
+			newPressure := currentPressure + distanceAndOpen*currentFlow
+			newFlow := currentFlow + pressures[v]
+			possibleScore := solveRecur(matrix, pressures, newTime, newPressure, newFlow, v, removeFromList(remaining, v), limit)
+			if possibleScore > max {
+				max = possibleScore
+			}
+		}
+	}
+
+	return max
+}
+
+func removeFromList(in []string, v string) []string {
+	new := []string{}
+	for _, i := range in {
+		if i != v {
+			new = append(new, i)
+		}
+	}
+	return new
+}
+
+func calcReachabilityMatrix(reachability map[string][]string, mappingInt map[string]int) map[string]map[string]int {
+	graph := dijkstra.NewGraph()
+
+	for k := range reachability {
+		graph.AddVertex(mappingInt[k])
+	}
+
+	for k, v := range reachability {
+		for _, l := range v {
+			graph.AddArc(mappingInt[k], mappingInt[l], 1)
+		}
+	}
+
+	matrix := map[string]map[string]int{}
+	for k1, v1 := range mappingInt {
+		matrix[k1] = map[string]int{}
+		for k2, v2 := range mappingInt {
+			best, _ := graph.Shortest(v1, v2)
+			matrix[k1][k2] = int(best.Distance)
+		}
+	}
+
+	return matrix
+}
+
+func createOpposite(all []string, partial []string) []string {
+	new := []string{}
+
+outer:
+	for _, v := range all {
+		for _, w := range partial {
+			if v == w {
+				continue outer
+			}
+		}
+		new = append(new, v)
+	}
+
+	return new
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module example/aoc
 
 go 1.19
+
+require (
+	github.com/RyanCarrier/dijkstra v1.3.0
+	github.com/ernestosuarez/itertools v0.0.0-20190516153236-40a02c159e7b
+)
