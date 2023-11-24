1bea5fde72a5532e4d6690f7fe9984adf9639410
DDQuin <ddquin420@gmail.com>
2023-11-24 21:10:51 +0000

    use max/min as base for flood fill

--- day18.go
+++ day18.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -30,36 +31,93 @@ func getPointsMap(lines []string) (map[Point3]Point3, Point3) {
 	return points, startingPoint
 }
 
-func floodFill(pointsMap map[Point3]Point3, point Point3, directions []Point3, visited map[Point3]Point3, distance int) {
-	if len(visited) >= distance {
-		return
-	}
+func floodFillRange(pointsMap map[Point3]Point3, point Point3, directions []Point3, visited map[Point3]Point3, max int, min int) bool {
 	_, isVisited := visited[Point3{x: point.x, y: point.y, z: point.z}]
 	if isVisited {
-		return
+		return false
 	}
 	_, exists := pointsMap[Point3{x: point.x, y: point.y, z: point.z}]
 	if exists {
-		return
+		return false
+	}
+
+	if point.x <= min || point.y <= min || point.z <= min {
+		return true
+	}
+
+	if point.x >= max || point.y >= max || point.z >= max {
+		return true
 	}
 	visited[Point3{x: point.x, y: point.y, z: point.z}] = Point3{x: point.x, y: point.y, z: point.z}
 
 	for _, direction := range directions {
 		nextPoint := Point3{x: point.x + direction.x, y: point.y + direction.y, z: point.z + direction.z}
-		floodFill(pointsMap, nextPoint, directions, visited, distance)
+		result := floodFillRange(pointsMap, nextPoint, directions, visited, max, min)
+		if result {
+			return result
+		}
 	}
+	return false
 
 }
 
-func checkSurfaceArea(point Point3, pointsMap map[Point3]Point3, directions []Point3, distance int) int {
+func getMinMaxPoints(pointMap map[Point3]Point3) (int, int) {
+	maxX := math.MinInt32
+	maxY := math.MinInt32
+	maxZ := math.MinInt32
+	minX := math.MaxInt32
+	minY := math.MaxInt32
+	minZ := math.MaxInt32
+	for point, _ := range pointMap {
+		if point.x < minX {
+			minX = point.x
+		}
+		if point.y < minY {
+			minY = point.y
+		}
+		if point.z < minZ {
+			minZ = point.z
+		}
+
+		if point.x > maxX {
+			maxX = point.x
+		}
+		if point.y > maxY {
+			maxY = point.y
+		}
+		if point.z > maxZ {
+			maxZ = point.z
+		}
+
+	}
+
+	min := minX
+	if minY < min {
+		min = minY
+	}
+	if minZ < min {
+		min = minZ
+	}
+
+	max := maxX
+	if maxY > max {
+		max = maxY
+	}
+	if maxZ > max {
+		max = maxZ
+	}
+	return max, min
+}
+
+func checkSurfaceArea(point Point3, pointsMap map[Point3]Point3, directions []Point3, max int, min int) int {
 	surface := 0
 
 	for _, direction := range directions {
 		_, exists := pointsMap[Point3{x: point.x + direction.x, y: point.y + direction.y, z: point.z + direction.z}]
 		if !exists {
 			visited := make(map[Point3]Point3, 0)
-			floodFill(pointsMap, Point3{x: point.x + direction.x, y: point.y + direction.y, z: point.z + direction.z}, directions, visited, distance)
-			if len(visited) >= distance {
+			result := floodFillRange(pointsMap, Point3{x: point.x + direction.x, y: point.y + direction.y, z: point.z + direction.z}, directions, visited, max, min)
+			if result {
 				surface++
 			}
 
@@ -70,8 +128,6 @@ func checkSurfaceArea(point Point3, pointsMap map[Point3]Point3, directions []Po
 }
 
 func Day18() {
-	//Could improbe by creating max andd min points and then when flood fill check if point is
-	//ooutside max and min cube
 	lines, err := ReadLines("resources/day18input.txt")
 	if err != nil {
 		log.Fatalf("readLines: %s", err)
@@ -85,11 +141,13 @@ func Day18() {
 	directions = append(directions, Point3{x: 0, y: -1, z: 0})
 	directions = append(directions, Point3{x: 0, y: 0, z: 1})
 	directions = append(directions, Point3{x: 0, y: 0, z: -1})
+
 	total := 0
-	distance := 1003
+	max, min := getMinMaxPoints(points)
+
 	for k, _ := range points {
 
-		total = total + checkSurfaceArea(k, points, directions, distance)
+		total = total + checkSurfaceArea(k, points, directions, max, min)
 	}
 
 	fmt.Println("There are ", total)
