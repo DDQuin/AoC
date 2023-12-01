380ff8df5269da36c9b7f2bf65c2213c53a57282
DDQuin <ddquin420@gmail.com>
2023-12-01 15:48:48 +0000

    day12023

--- 2023/1/1.go
+++ 2023/1/1.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func ReadLines(path string) ([]string, error) {
@@ -21,10 +24,56 @@ func ReadLines(path string) ([]string, error) {
 	return lines, scanner.Err()
 }
 
+func getNumFromString(line string) string {
+	digitMaps := map[string]string{"one": "1", "two": "2", "three": "3", "four": "4",
+		"five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+	for key, value := range digitMaps {
+		if strings.Contains(line, key) {
+			return value
+		}
+
+	}
+	return ""
+
+}
+
+func getcalValue(line string) int {
+	firstDigit := ""
+	secondDigit := ""
+
+	for i := 0; i < len(line); i++ {
+		secondIndex := len(line) - i - 1
+		if firstDigit == "" {
+			if line[i] >= '0' && line[i] <= '9' {
+				firstDigit = string(line[i])
+			} else {
+				firstDigit = getNumFromString(line[0 : i+1])
+			}
+		}
+		if secondDigit == "" {
+			if line[secondIndex] >= '0' && line[secondIndex] <= '9' {
+				secondDigit = string(line[secondIndex])
+			} else {
+				secondDigit = getNumFromString(line[secondIndex:])
+			}
+		}
+	}
+	calVal, _ := strconv.Atoi(firstDigit + secondDigit)
+	return calVal
+}
+
 func main() {
-	lines, err := ReadLines("test.txt")
+	lines, err := ReadLines("input.txt")
 	if err != nil {
 		log.Fatalf("readLines: %s", err)
 	}
+
+	total := 0
+	for _, line := range lines {
+		num := getcalValue(line)
+		total += num
+	}
+
+	fmt.Println("Total is", total)
 
 }
